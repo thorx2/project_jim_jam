84d36619aad18dfa268b04dfc74692eea99f95f6
Gaudham P <7636788+thorx2@users.noreply.github.com>
2024-01-21 08:31:06 +0530

    add logic for game viewport scale to high res viewport

--- CoreGame/Systems/GameManager.cs
+++ CoreGame/Systems/GameManager.cs
@@ -3,15 +3,34 @@ using System;
 
 namespace CoreGame.GameSystems;
 
-public partial class GameManager : Node
+public partial class GameManager : Node2D
 {
-    // Called when the node enters the scene tree for the first time.
+
+    #region Viewport Scaling
+    /// <summary>
+    /// Spawn game play elements within this VP as the parent.
+    /// </summary>
+    [Export]
+    private Viewport gameViewport;
+
+    [Export]
+    private SubViewportContainer gameVPContainer;
+
+    private Vector2 scaling;
+
+    private Vector2 rootVPSize;
+    #endregion
+
     public override void _Ready()
     {
+        ScaleGameplayViewportToParent();
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
+    private void ScaleGameplayViewportToParent()
     {
+        rootVPSize = GetViewportRect().Size;
+        scaling = rootVPSize / gameViewport.GetVisibleRect().Size;
+        gameVPContainer.Scale = scaling;
     }
+
 }
