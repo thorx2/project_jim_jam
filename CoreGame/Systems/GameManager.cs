67cdce9b4233be1d1a0257cf035ca447269be875
HritikDutta <dutta.hritik@gmail.com>
2024-01-25 16:43:30 +0530

    Merged art branch to main branch

--- CoreGame/Systems/GameManager.cs
+++ CoreGame/Systems/GameManager.cs
@@ -5,13 +5,13 @@ namespace CoreGame.GameSystems;
 
 public partial class GameManager : Node
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
 }
