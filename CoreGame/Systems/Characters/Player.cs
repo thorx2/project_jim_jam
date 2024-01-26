ab1b5bb1b3e8fb41cbc6c816ab6716f009f2d58c
HritikDutta <dutta.hritik@gmail.com>
2024-01-27 03:39:51 +0530

    Diagonal Movement Negation

--- CoreGame/Systems/Characters/Player.cs
+++ CoreGame/Systems/Characters/Player.cs
@@ -3,34 +3,34 @@ using Godot;
 
 public partial class Player : Character
 {
-    [Export]
-    private AnimatedSprite2D characterVisual;
-    [ExportSubgroup("Debug Display Data")]
-    [Export]
-    public EPlayerState CurrentPlayerState;
+	[Export]
+	private AnimatedSprite2D characterVisual;
+	[ExportSubgroup("Debug Display Data")]
+	[Export]
+	public EPlayerState CurrentPlayerState;
 
-    [Export]
-    private MovementSubsystem movementSubsystem;
-    
-    public override void _Ready()
-    {
-        base._Ready();
-        MasterSignalBus.GetInstance.LoadMapEvent?.Invoke(0);
-        CurrentPlayerState = EPlayerState.EPlayerWalking;
-    }
+	[Export]
+	private MovementSubsystem movementSubsystem;
+	
+	public override void _Ready()
+	{
+		base._Ready();
+		MasterSignalBus.GetInstance.LoadMapEvent?.Invoke(0);
+		CurrentPlayerState = EPlayerState.EPlayerWalking;
+	}
 
-    public void ResetGameCharacter(Vector2 atPosition)
-    {
-        GlobalPosition = atPosition;
-        movementSubsystem.SetPhysicsProcess(false);
-        movementSubsystem.SetProcess(false);
-        movementSubsystem.ForceSetTargetPosition(atPosition);
-    }
+	public void ResetGameCharacter(Vector2 atPosition)
+	{
+		GlobalPosition = atPosition;
+		movementSubsystem.SetPhysicsProcess(false);
+		movementSubsystem.SetProcess(false);
+		movementSubsystem.ForceSetTargetPosition(atPosition);
+	}
 
-    public override void SnapCharacterToTileOnMap(Vector2 pos)
-    {
-        movementSubsystem.SetPhysicsProcess(true);
-        movementSubsystem.SetProcess(true);
-        base.SnapCharacterToTileOnMap(pos);
-    }
+	public override void SnapCharacterToTileOnMap(Vector2 pos)
+	{
+		movementSubsystem.SetPhysicsProcess(true);
+		movementSubsystem.SetProcess(true);
+		base.SnapCharacterToTileOnMap(pos);
+	}
 }
