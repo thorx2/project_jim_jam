712cb0b1852c5ab438222dfe0413886015a87b8e
Gaudham P <7636788+thorx2@users.noreply.github.com>
2024-01-27 03:28:43 +0530

    Add more configuration stuff for designers

--- CoreGame/Systems/GameRuntimeParameters.cs
+++ CoreGame/Systems/GameRuntimeParameters.cs
@@ -2,8 +2,12 @@ using System;
 
 public static class GameRuntimeParameters
 {
+    public static float BurstCollateralSpread;
     public static float MaxTolerableSpread;
     public static float GossipSpread;
+
+    public static float ColorFailSpread;
+    public static float GreyFailSpread;
     
     public static int GameDay;
 
